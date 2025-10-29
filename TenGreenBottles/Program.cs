6e56f3c66b16019807a162f1330c973f76eb49f2
Venator <71978022+ImperialVenator@users.noreply.github.com>
2025-10-29 09:20:17 +0000

    Refining Ten Green Bottles program
    bottle counting logic and improve output formatting.

--- TenGreenBottles/Program.cs
+++ TenGreenBottles/Program.cs
@@ -1,36 +1,43 @@
-// TenGreenBottles.cs
+
 using System;
 using System.Text;
 
+Console.WriteLine("    ");
 Console.WriteLine("This is the ten green bottles song!");
 
-static string BottleBottles(int x)
+static string numBottles(int x)
 {
     if (x == 1)
     {
         return "bottle";
     }
-    return "bottles";
+    else
+    {
+        return "bottles";
+    }
+    
 }
 
 bool playAgain = true;
 
 while (playAgain == true)
 {
+    Console.WriteLine("   ");
     Console.WriteLine("What is the number of green bottles you want to start with?");
     int num = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine();
 
     for (int i = num; i > 0; i--)
     {
-        Console.WriteLine(i + " green " + BottleBottles(i) + " hanging on the wall");
-        Console.WriteLine(i + " green " + BottleBottles(i) + " hanging on the wall,");
+        Console.WriteLine(i + " green " + numBottles(i) + " hanging on the wall");
+        Console.WriteLine(i + " green " + numBottles(i) + " hanging on the wall,");
         Console.WriteLine("And if one green bottle should accidentally fall,");
-        Console.WriteLine("There'll be " + (i - 1) + " green " + BottleBottles(i - 1) + " hanging on the wall");
+        Console.WriteLine("There'll be " + (i - 1) + " green " + numBottles(i - 1) + " hanging on the wall");
+        Console.WriteLine("    ");
     }
 
     Console.WriteLine("Do you want to play again? (yes/no)");
-    string answer = Console.ReadLine();
+    string answer = Console.ReadLine() ?? string.Empty;
     switch (answer.ToLower())
     {
         case "yes":
@@ -43,5 +50,12 @@ while (playAgain == true)
             playAgain = false;
             break;
     }
+
 }
 
+//This is how it works:
+
+//The switch expression is evaluated once
+//The value of the expression is compared with the values of each case
+//If there is a match, the associated block of code is executed
+//The break and default keywords will be described later in this chapter
